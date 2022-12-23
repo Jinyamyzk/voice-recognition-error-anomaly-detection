f0c4fc361752c67d53a2580ca03417b48b7df7c5
Jinyamyzk <63580543+Jinyamyzk@users.noreply.github.com>
2022-12-23 14:10:40 +0900

    Modify

--- bert_anomaly_detection.py
+++ bert_anomaly_detection.py
@@ -5,14 +5,14 @@ from torch import nn
 
 import pandas as pd
 import copy
+import warnings
+warnings.simplefilter("ignore")
 
-def fill_mask(model,  tokenizer, text):
-    inputs = tokenizer(text, return_tensors="pt")
+def fill_mask(model,  tokenizer, masked_text):
+    inputs = tokenizer(masked_text, return_tensors="pt").to(device)
     mask_token_index = (inputs.input_ids == tokenizer.mask_token_id)[0].nonzero(as_tuple=True)[0]
-    words = [tokenizer.decode(id) for id in inputs.input_ids]
-    # print(words)
     with torch.no_grad():
-        output = model(**inputs, output_attentions=True)
+        output = model(**inputs)
     predicted_token_ids = output.logits[0, mask_token_index]
     return predicted_token_ids
 
@@ -26,11 +26,10 @@ def eval_model(text, label, model, tokenizer, softmax):
         token_id = tokenizer.convert_tokens_to_ids(target)
         tokens_[i] = MASK_TOKEN
         masked_text = "".join(tokens_).replace('#', '')
-        input =  masked_text 
-        probes = fill_mask(model, tokenizer, input)
-        probes = softmax(probes)
-        predicted = torch.topk(probes[0], k=50)
-        result.append(int(token_id in predicted.indices.tolist()))
+        preds = fill_mask(model, tokenizer, masked_text)
+        probes = softmax(preds)
+        top_k = torch.topk(probes[0], k=50)
+        result.append(int(token_id in top_k.indices.tolist()))
     result = torch.tensor(result)
     len_label = len(label) 
     label = torch.tensor(label)
@@ -48,8 +47,8 @@ def main():
     model = BertForMaskedLM.from_pretrained("cl-tohoku/bert-base-japanese-whole-word-masking")
     tokenizer = BertJapaneseTokenizer.from_pretrained("cl-tohoku/bert-base-japanese-whole-word-masking")
     softmax = nn.Softmax(dim=1)
-    
 
+    model.to(device)
     model.eval()
 
     text = "どうして男の人って、テレビをつけたまま寝るんだろう契約社員の佳子さんが、とある飲み会でぽろっとつぶやいたところ、その場にいたほぼ全員の女性が、そうそうホント、やくめてほしいよねからといっせいやくにす同調したという周囲の女性たちの反応に、TVをつけっぱなしで寝る男にイラッとしているのは自分だけではない、と意を強くした佳子さん私の彼は、すごくテレビ好きなんですよ"
@@ -61,4 +60,8 @@ def main():
     print(f"Accuracy: {accuracy}, Recall: {recall}")
 
 if __name__ == "__main__":
+    # GPUが使えるかを確認
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("使用デバイス：", device)
+    print('-----start-------')
     main()
