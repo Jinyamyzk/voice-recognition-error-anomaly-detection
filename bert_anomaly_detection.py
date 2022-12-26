494f05bc83414d2aecb5667178bdca523c8132b5
Jinyamyzk <63580543+Jinyamyzk@users.noreply.github.com>
2022-12-26 12:29:24 +0900

    Modify

--- bert_anomaly_detection.py
+++ bert_anomaly_detection.py
@@ -94,5 +94,5 @@ if __name__ == "__main__":
     print("使用デバイス：", device)
     print('-----start-------')
     df = pd.read_csv("livedoor_noised_data.tsv", sep="\t", names=["text", "label"])
-    # df = df.head(5) # For debugging
+    df = df.head(5) # For debugging
     main(df)
